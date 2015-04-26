e30045715418ed8742d614abae6c77dbdca24ffd
Will <whp3652@rit.edu>
2015-04-26 17:19:22 -0400

    Wire up first view

--- back/lib/app.js
+++ back/lib/app.js
@@ -1,9 +1,33 @@
 (function() {
-  var express, route;
+  var express, http_port, path, route, server;
 
-  express = require("express");
+  express = require('express');
 
-  route = require("./router.coffee");
+  route = require('./router.coffee');
+
+  path = require('path');
+
+  http_port = 3000;
+
+  app.set('views', './views');
+
+  app.set('view engine', 'jade');
+
+  app.disable('x-powered-by');
+
+  server = app.listen(config.http_port, function(err) {
+    if (err) {
+      return console.log(err);
+    } else {
+      return console.log("===== Serving on port " + http_port + " ======");
+    }
+  });
+
+  app.get('/', function(req, res) {
+    return res.render('index', {
+      title: 'Project'
+    });
+  });
 
 }).call(this);
 
--- back/src/app.coffee
+++ back/src/app.coffee
@@ -1,3 +1,17 @@
-express = require("express")
-route   = require("./router.coffee")
+express   = require('express')
+route     = require('./router.coffee')
+path      = require('path')
+http_port = 3000
 
+app.set('views', './views')
+app.set('view engine', 'jade')
+app.disable('x-powered-by')
+
+server = app.listen(config.http_port, (err) ->
+	if(err)
+		console.log(err)
+	else
+		console.log("===== Serving on port " + http_port + " ======"))
+
+app.get('/', (req, res) ->
+  res.render('index', { title: 'Project' }))
