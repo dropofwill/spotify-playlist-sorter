d306e73e08ab87c1684f5cf90325c8d65c9444a0
Will <whp3652@rit.edu>
2015-05-01 01:47:41 -0400

    remove duplicate file and reposition spotify stuff where it should be

---
+++ back/lib/spotify.js
@@ -0,0 +1,45 @@
+(function() {
+  var spotify;
+
+  spotify = exports;
+
+
+  /*
+   * Returns a URL string for phase 1 of the OAuth 2 process
+   * Requires a string state parameter
+   * Optionally pass a string host, string path, or array of scopes
+   */
+
+  spotify.auth_builder = function(state, host, path, scopes) {
+    var scope;
+    if (host == null) {
+      host = config.accounts_host;
+    }
+    if (path == null) {
+      path = config.auth_path;
+    }
+    if (scopes == null) {
+      scopes = ['user-read-private', 'user-read-email'];
+    }
+    scope = scopes.join(" ");
+    return url.format({
+      protocol: 'https',
+      hostname: host,
+      pathname: path,
+      query: {
+        response_type: 'code',
+        client_id: config.client_id,
+        redirect_uri: config.redirect_uri,
+        state: state,
+        scope: scope
+      }
+    });
+  };
+
+  spotify.build_token_req = function() {};
+
+  spotify.build_query_req = function() {};
+
+}).call(this);
+
+//# sourceMappingURL=spotify.js.map
---
+++ back/src/spotify.coffee
@@ -0,0 +1,26 @@
+spotify = exports
+
+###
+# Returns a URL string for phase 1 of the OAuth 2 process
+# Requires a string state parameter
+# Optionally pass a string host, string path, or array of scopes
+###
+spotify.auth_builder = (state, host=config.accounts_host, path=config.auth_path,
+                      scopes=['user-read-private', 'user-read-email']) ->
+  scope = scopes.join(" ")
+
+  url.format(
+    protocol: 'https'
+    hostname: host
+    pathname: path
+    query:
+      response_type:  'code'
+      client_id:      config.client_id
+      redirect_uri:   config.redirect_uri
+      state:          state
+      scope:          scope)
+
+
+spotify.build_token_req = () ->
+
+spotify.build_query_req = () ->
